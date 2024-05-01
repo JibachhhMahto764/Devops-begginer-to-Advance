5173b481a87d40d7eb649d885b186a81e9dfaaf8
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-01 12:49:30 +0545

    Create Block.go

---
+++ Go-Lang/Program/Block/Block.go
@@ -0,0 +1,22 @@
+Book =>
+
+        {
+          // outer block 
+
+          {
+            // inner block 
+            }
+          }
+
+
+package main
+import "fmt"
+func main() {
+  city:= "Pokhara"
+  {
+    Country:= "Nepal"
+    fmt.PrintIn(Coutry)
+    fmt.PrintIn(city)
+    }
+  fmt.PrintIn(city)
+  }
