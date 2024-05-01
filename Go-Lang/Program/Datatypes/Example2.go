1401b5d9f0f716b7718b4089d81068793b4d762b
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-01 12:38:11 +0545

    Create Example2.go

---
+++ Go-Lang/Program/Datatypes/Example2.go
@@ -0,0 +1,11 @@
+package main
+import("fmt")
+func main(){
+  var name string = "jibachh"
+  var i int = 78
+  fmt.Printf("hey,%v! you have scored %d/100 in physics", name,i)
+  }
+
+
+cmd=:
+go run main.go
