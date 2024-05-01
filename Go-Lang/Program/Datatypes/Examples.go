6f35bcd13a7680f9abc22460a4f026207af5c431
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-01 12:33:25 +0545

    Create Examples.go

---
+++ Go-Lang/Program/Datatypes/Examples.go
@@ -0,0 +1,26 @@
+Interger => int
+Uint => Unsigned => 0 To positive number
+int => singed => negative To positive
+
+float32 
+float64 =>  In Go we are genrally use float64 for floatting value such as 45.5566
+
+
+string  =>"abs"
+
+Boolean => bool
+
+syntax =>
+     var s string = "hello jibachh!!."
+
+example=>
+            package main
+           import ("fmt")
+           func main(){ 
+             var greeting string = "hello jibachh!!"
+             fmt.Println(greeting)
+             }
+
+
+cmd:--
+go run Examples.go
