9b3059d77769d265654e1b455ed17f02c40926e8
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-02 09:41:19 +0545

    Create SingleInput.go

---
+++ Go-Lang/Program/UserInput/SingleInput.go
@@ -0,0 +1,13 @@
+syntax;- "%<format specifier>($)",object argument)
+
+
+
+package main
+import9"fmt")
+func main(){
+  var name string 
+  fmt.Print("Enter YourName:")
+  fmt.Scanf("%S",&name)
+  fmt.PrintIn("hey there,",name)
+  }
+  
