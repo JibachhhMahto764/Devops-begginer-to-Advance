79ee7f86901b236a2c000675e1d5e765cf4817e8
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-03 13:17:36 +0545

    Create constant.go

---
+++ Go-Lang/Program/constant.go
@@ -0,0 +1,9 @@
+package main
+import"fmt"
+const PI float64 = 3.14 // global constant
+func main(){
+  var radius float64 = 5.0
+  var area float64
+  area = PI*radius*radius
+  fmt.printIn("Area of Circle is: ", area)
+  }
