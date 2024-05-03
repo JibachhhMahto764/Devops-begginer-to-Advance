e97bdb3305b88c9466d9b2eb6311a6635d21341a
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-03 12:33:23 +0545

    Create Usingreflect.go

---
+++ Go-Lang/Program/TypeofVariable/Usingreflect.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+import"fmt"
+func main(){
+fmt.printf("type:%v \n",reflect.Typeof(100)
+fmt.printf(type:%v \n", reflect.typeof("hero")
+fmt.printf("type:%v \n",reflect.typeof(46.0)
+fmt.printf("type:%v \n",reflect.typeof(true)
+           }
+
+  
