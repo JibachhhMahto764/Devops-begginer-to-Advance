a782e70911ca88e9b08c55be9d79f5808a9d2951
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-03 13:55:09 +0545

    Create ArithmaticOP.go

---
+++ Go-Lang/Program/Operators/ArithmaticOP.go
@@ -0,0 +1,13 @@
+package main
+import"fmt"
+func main(){
+  var a,b float64 = 97.988,44.8788
+   fmt.printf("%.2f",a-b)
+   fmt.printf("%.2f",a+b)
+   fmt.printf("%.2f",a*b)
+   fmt.printf("%.2f",a%b)
+   fmt.printf("%.2f",a/b)
+  fmt.printf("%.2f",a++b)
+  fmt.printf("%.2f",a--b)
+  }
+  
