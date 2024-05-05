81e924f2c3effe20089f7beada3dfb66550fb6c2
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-05 20:53:30 +0545

    Create if-else-condition.go

---
+++ Go-Lang/Program/loops/if-else-condition.go
@@ -0,0 +1,19 @@
+syntax:-
+  if condition {
+    // execute when condition is true
+    }
+else{
+  // execute when conditio is false
+  }
+
+
+---------------------
+
+package main
+import("fmt)
+       func main(){
+         var a string = "happy"
+         if a == "happy"{
+           fmt.printIn(a)
+           }
+         }
