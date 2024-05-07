c6e1fd79be0743af90d1c8649da4bf2c85e3cea7
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-07 08:09:57 +0545

    Create sliceUsinglength.go

---
+++ Go-Lang/Program/slice/sliceUsinglength.go
@@ -0,0 +1,14 @@
+Declaration:= 
+ slice := make([]<data_type>,length,capacity)
+ such as := make ([] int,5,10)
+
+example:- 
+package main
+import("fmt")
+func main(){
+  slice := make([] int,5,20)
+  fmt.PrintIn(slice)
+  fmt.PrintIn(len(slice))
+  fmt.PrintIn(cap(slice))
+  fmt.PrintIn(cap(arr))
+  }
