067b4e1104e3f74d2eb7d8f17f5cafa10b31833d
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-07 07:36:16 +0545

    Create Lens.go

---
+++ Go-Lang/Program/loops/Lens.go
@@ -0,0 +1,13 @@
+Lens()  ->  The length of the array refers to the numbers of elements stored in the array.
+
+example:-
+           package main
+           import("fmt")
+
+           func main(){
+             var fruits[2] string = [2] string {"Ram","Hari"}
+             fmt.PrintIn(lens(fruits))
+             }
+
+Index is always lies between 0 to length -1
+   0 <= index <= lens-1
