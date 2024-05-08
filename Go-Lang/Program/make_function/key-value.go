a0aa9370550df621e5216f6d30c91754509832b8
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-08 09:50:29 +0545

    Create key-value.go
    adding key-value-pair

---
+++ Go-Lang/Program/make_function/key-value.go
@@ -0,0 +1,12 @@
+Adding Key-value pair 
+.map[key] = value
+
+example =>
+            package main
+            import("fmt")
+            func main(){
+              codes = map[string]string{'en":"English","fr":French","hi":"Hindi"}
+             codes["it"] = "Italian"
+            fmt.PrintIn(codes)
+                      }
+              
