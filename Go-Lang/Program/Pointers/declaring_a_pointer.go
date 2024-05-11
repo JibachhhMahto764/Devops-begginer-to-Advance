dcbce45a194bdbe5c531db74021ec2cd3ed02ab2
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-11 22:00:57 +0545

    Create declaring_a_pointer.go

---
+++ Go-Lang/Program/Pointers/declaring_a_pointer.go
@@ -0,0 +1,21 @@
+*********Declaring_a_pointer************
+
+syntax => var<pinter_name>*<<data_type>
+
+such as =>  var ptr_i *int
+            var ptr_s * string
+
+
+
+-----------program--------
+
+package main
+import"fmt"
+func main(){
+  var i * int
+  var s * string
+  fmt.PrintIn(i)
+  fmt.PrintIn(s)
+  }
+
+
