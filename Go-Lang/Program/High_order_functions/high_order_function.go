71e7b68ddd6a2dafd3761e4df2bdd7e47f82c07c
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-11 15:10:34 +0545

    Create high_order_function.go

---
+++ Go-Lang/Program/High_order_functions/high_order_function.go
@@ -0,0 +1,20 @@
+package main
+import"fmt"
+func main() {
+  var query int
+  var radius float64
+  fmt.Print("enter the radius of the circle:"
+            fmt.Scanf("%f",&radius)
+            fmt.printf("enter \n 1-area \n 2-parimeter \n3-diameter:")
+            fmt.Scanf(%d",&query)
+                      if query ==1 {
+                        fmt.PrintIn(Result:",calcArea(radius))
+                                    }else if query ==2{
+                                      fmt.PrintIn("Result:",calcperimeter(radius))
+                                      }else if query ==3{
+                                      fmt.PrintIn("Result:",calcDiameter(radius))
+                                      }else{
+                                      fmt.PrintIn("invalid query")
+                                      }
+                                    }
+                                    
