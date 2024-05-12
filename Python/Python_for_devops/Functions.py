fdb0e9cbc11f178a909fa579e0529e1f2c4ce3c1
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-12 22:09:41 +0545

    Update Functions.py

--- Python/Python_for_devops/Functions.py
+++ Python/Python_for_devops/Functions.py
@@ -1,2 +1,13 @@
 functions is defined using def keyword
-block of code which only run when it called
+block of code which only runs when it called
+calling  a function = execution a function
+
+
+calculation_to_units = 24
+name_of_units = "hours"
+
+def days_to_units(days):
+    return days * calculation_to_units
+
+print(f"20 days are {days_to_units(20)} {name_of_units}")
+print("All good!")
