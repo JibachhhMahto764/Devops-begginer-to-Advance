7508b06b8f32b0652557ac2917ad1dda0c6ba436
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-12 20:57:28 +0545

    Update variable.py

--- Python/Python_for_devops/variable.py
+++ Python/Python_for_devops/variable.py
@@ -1,4 +1,4 @@
-// python is dynamic programming language so not need to define the types of variables
+// python is a dynamic programming language so no need to define the types of variables
 
 calculation_to_second = 24*60*60
 print(f"100 days are equal {100*calculation_to_second} minutes")
@@ -6,3 +6,14 @@ print(f"10 days are equal {10*calculation_to_second} minutes")
 print(f"120 days are equal {120*calculation_to_second} minutes")
 print(f"130 days are equal {130*calculation_to_second} minutes")
 print(f"150 days are equal {150*calculation_to_second} minutes")
+
+another example of variables.....
+
+calculation_to_units = 24*60*60
+name_of_units = "seconds"
+
+print(f"100 days are equal {100* calculation_to_units}{name_of_units} ")
+print(f"10 days are equal {10* calculation_to_units} {name_of_units}")
+print(f"120 days are equal {120* calculation_to_units} {name_of_units}")
+print(f"130 days are equal {130* calculation_to_units} {name_of_units}")
+print(f"150 days are equal {150* calculation_to_units} {name_of_units}")
