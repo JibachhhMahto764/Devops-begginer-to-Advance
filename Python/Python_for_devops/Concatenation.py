7fabdd7c03a73a28122b82470d2fc71254dc581f
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-12 20:36:40 +0545

    Create Concatenation.py

---
+++ Python/Python_for_devops/Concatenation.py
@@ -0,0 +1,7 @@
+# python  concatenation 
+print("20 days are equal to "+ str(60) + " minutes")
+print(f"100 days are equal {100*24*60} minutes")
+print(f"10 days are equal {10*24*60} minutes")
+print(f"120 days are equal {120*24*60} minutes")
+print(f"130 days are equal {130*24*60} minutes")
+print(f"150 days are equal {150*24*60} minutes")
