1f3919b8794d2650f8a6323c1d34a6c3d8fe3a83
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-13 12:51:18 +0545

    Create slice_passing_by_reference.go

---
+++ Go-Lang/Program/slice/slice_passing_by_reference.go
@@ -0,0 +1,12 @@
+package main
+import"fmt"
+
+func modify(s[] int | {
+  s[0] = 100
+  }
+            func main {
+              slice := [] int { 10,20,30}
+              fmt.PrintIn(slice)
+              modify(slice)
+              fmt.PrintIn(slice)
+              }
