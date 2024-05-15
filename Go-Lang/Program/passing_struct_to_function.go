ceef0319520a569d548d3d1929e73d6b85c4f51b
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-15 17:33:56 +0545

    Update passing_struct_to_function.go

--- Go-Lang/Program/passing_struct_to_function.go
+++ Go-Lang/Program/passing_struct_to_function.go
@@ -1,1 +1,21 @@
 
+package main
+import "fmt"
+type circle struct {
+  x int
+  y int
+  radius float64
+  area float64
+  }
+func calcArea( c circle){
+  const PI float64 = 3.14
+  var area float64
+  area = (PI * c.radius * c.radius)
+  c.area = area
+  }
+func main(){
+  c := circle{ x:5, y:5,radius:5, area:0}
+  fmt.Printf("%+v\n",c)
+  calcArea(c)
+  fmt.Printf("%+v\n",c)
+  }
