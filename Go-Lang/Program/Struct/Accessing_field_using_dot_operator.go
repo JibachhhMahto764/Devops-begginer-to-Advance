9587d28c2976da071974dbf9837043a94e2ae395
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-05-15 17:36:37 +0545

    Create Accessing_field_using_dot_operator.go

---
+++ Go-Lang/Program/Struct/Accessing_field_using_dot_operator.go
@@ -0,0 +1,14 @@
+package main
+import"fmt"
+type circle struct{
+  x int
+  y int
+  radius int
+  }
+func main(){
+  var c circle
+  c.x = 5
+  c.y = 5
+  c .rafius = 5
+  fmt.Printf("%+v\n",c)
+  }
