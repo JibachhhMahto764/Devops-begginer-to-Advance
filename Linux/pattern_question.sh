0169ddfde3ac1c9655debccbca0052fc5b977ba0
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2025-02-18 19:22:43 +0545

    Create pattern_question.sh
    This is the pattern question1

---
+++ Linux/pattern_question.sh
@@ -0,0 +1,16 @@
+
+##### pattern question in bash scripting
+#!/bin/sh
+
+a=0
+while [ "$a" -lt 10 ]  # this is loop1
+do
+  b="$a"
+  while [ "$b" -ge 0 ] # this is loop2
+  do
+  echo -n "$b"
+  b='expr $b - 1'
+  done
+  echo 
+  a='expr $a + 1'
+  done
