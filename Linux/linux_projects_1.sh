e4773f585aa4e1038d7ade5ba14d5dad852e064f
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2025-03-14 21:53:35 +0545

    Update linux_projects_1.sh
    adding some comment on this project code

--- Linux/linux_projects_1.sh
+++ Linux/linux_projects_1.sh
@@ -3,12 +3,12 @@
 #simple Password Generator
 echo"Welcome to Password Generator"
 
-sleep 2
+sleep 2        #this is the sleep program for 2 sec
 
 echo"Please Enter The Length Of The Password:-"
-read PASS_LENGTH
+read PASS_LENGTH    #input from user
 
-for p in $(seq 1 );
+for p in $(seq 1 );  # for loop and $ is used for varying the data 
 do
-   openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+   openssl rand -base64 48 | cut -c1-$PASS_LENGTH    # this is for generating the random password with base 64
 done
